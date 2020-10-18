6d79e15a3f5c64147d73f3db04df65f8f2cec9fa
alexandra.brehier <alexandra.brehier@talentcoin.tech>
2020-10-18 16:37:10 +0200

    Modification du HTML du site

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,4 +18,12 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
 ]
